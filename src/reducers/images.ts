e56b0dfd5eee3fac870c849bbc25f253c01b4800
Antonie Dimovski <antonie.dimovski@massive.co>
2020-03-09 10:56:05 +0100

    fifth commit

--- src/reducers/images.ts
+++ src/reducers/images.ts
@@ -5,7 +5,7 @@ export const imagesReducer = (state: Image[] = [], action: Action) => {
     case ActionTypes.fetchImages:
       return action.payload;
     case ActionTypes.deleteImage:
-      return state.filter((image: Image) => image.id !== action.payload);
+      return state.filter(({ id }: Image) => id !== action.payload);
     default:
       return state;
   }
